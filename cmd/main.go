b333b84544ac4ea8706c17d4fc13533ef7cd6251
dmitriy <kirichenko.dmitriy1977@gmail.com>
2026-06-02 16:00:12 +1000

    подключено middleware

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/Kirichenkodv/FiberPost/config"
+	"github.com/Kirichenkodv/FiberPost/internal/logger"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	config.Init()
+
+	cfg := config.Load()
+	log := logger.New(cfg.LogFormat, cfg.LogLevel)
+
 	app := fiber.New()
+	app.Use(logger.Middleware(log))
 	app.Listen(":8080")
 }
